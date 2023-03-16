470c83a0accab8c1d8fea8aed5372dc119204dea
Aubrey Carlon <aubreycarlon@Pauls-MacBook-Pro.local>
2023-03-15 20:51:17 -0700

    where

--- utils/fetchAPI.js
+++ utils/fetchAPI.js
@@ -6,7 +6,7 @@ export const fetchAPI = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'realtor.p.rapidapi.com',
-      'x-rapidapi-key': process.env.API_KEY,
+      'x-rapidapi-key': 'a64fc701afmshd4f1f8194fc0e83p1e95f6jsnb93e4a95570c',
     },
   });
     
